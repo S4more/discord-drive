8ee2b5948bf81cce428c42ae4335ff29aa66d7cd
S4more <guimsc@hotmail.com>
2021-04-03 03:48:46 -0400

    Accepts images and fixed naming bug

--- bot.py
+++ bot.py
@@ -10,25 +10,29 @@ import os
 bot = commands.Bot(command_prefix=('!salva '))
 drive = Drive()
 
-@bot.event
-async def on_command_error(ctx, error):
-    if hasattr(ctx.command, 'on_error'):
-        return
-
 class Utils(commands.Cog):
     @commands.command(pass_context=True, name="foto", brief="Guarda uma imagem no google drive")
     async def foto(self, ctx, *args):
-        title = '_'.join(args[:-1])
-        os.system(f"wget -O ./imgs/{title} {args[-1]}")
+        if 'http' in args[-1]:
+            link = args[-1]
+            title = '_'.join(args[:-1])
+        else:
+            try:
+                link = ctx.message.attachments[0].url
+                title = '_'.join(args)
+            except:
+                await ctx.send("Alguma coisa deu errado")
+                return;
+        os.system(f"wget -O ./imgs/{title} {link}")
         link = drive.upload_image(title, f"./imgs/{title}", ctx.author.name)
         os.system(f"rm ./imgs/{title}")
         await ctx.send(f"Foto salva com sucesso. Link: {link}")
 
-    @foto.error
-    async def foto_handler(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            if error.param.name == 'link':
-                await ctx.send("A sintaxe do comando esta errada. Usagem certa: `salva foto titulo link`")
+#    @foto.error
+#    async def foto_handler(self, ctx, error):
+#        if isinstance(error, commands.MissingRequiredArgument):
+#            if error.param.name == 'link':
+#                await ctx.send("A sintaxe do comando esta errada. Usagem certa: `salva foto titulo link`")
 
     @commands.command(pass_context=True, name="link", brief="Manda o link para a pasta do google drive.")
     async def link(self, ctx):
